bd1086f9187c4cad46b8227c317d066c77f67b50
Thekolllol90 <neosvarre@hotmail.se>
2020-02-04 19:28:58 +0100

    gjorde det jag orka lär mig inget på att följa en w3schools tutorial + blir bara irriterad på hur otydliga dom är och all skit och adjkfbncjkasdbgs

--- wesweb/firstprogram.js
+++ wesweb/firstprogram.js
@@ -1,6 +1,14 @@
 var http = require('http')
+var dt = require('./datum')
+var url = require('url')
+var fs = require('fs')
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type' : 'text/plain' })
-    res.end('Hello world!')
-}).listen(8000)
+    res.write("datumet och tiden är för nuvarandet: " + dt.myDateTime())
+    res.write(req.url)
+    var x = url.parse(req.url, true).query
+    var txt = x.year + " " + x.month
+    res.write(txt)
+    res.end()
+}).listen(8080)
